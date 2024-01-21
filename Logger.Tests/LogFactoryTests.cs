bdc7698bef4fd261cc98e4761e2fb27e678425bb
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-21 15:07:05 -0800

    created CreateLogger Method Check ClassName Test

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -5,5 +5,11 @@ namespace Logger.Tests;
 [TestClass]
 public class LogFactoryTests
 {
-
+    [TestMethod]
+    public void CreateLogger_CheckClassName_Success()
+    {
+        LogFactory LogFactory = new LogFactory();
+        BaseLogger FileLogger = LogFactory.CreateLogger("FileLogger");
+        Assert.AreEqual("FileLogger", FileLogger.ClassName);
+    }
 }
