4a60ba1c923e81453d22a151c6201998a401ad10
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-21 15:40:46 -0800

    Configure File logger

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -8,6 +8,11 @@ namespace Logger
 {
     public class FileLogger : BaseLogger
     {
+        private readonly string? _PathName;
+        public FileLogger(string filePath)
+        {
+            _PathName = filePath;
+        }
         public override void Log(LogLevel logLevel, string message)
         {
             throw new NotImplementedException();
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -5,11 +5,16 @@ namespace Logger;
 
 public class LogFactory
 {
+    private string? _PathName;
     public BaseLogger CreateLogger(string className)
     {
 
-        FileLogger FileLogger = new FileLogger();
+        FileLogger FileLogger = new FileLogger(_PathName!);
         FileLogger.ClassName = className;
         return FileLogger;
     }
+    public void ConfigureFileLogger(string pathName)
+        {
+            _PathName = pathName;
+        }
 }
