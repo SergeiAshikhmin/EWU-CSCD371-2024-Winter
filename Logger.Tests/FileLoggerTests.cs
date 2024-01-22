d87618aca8189d8eb5365187bab6f64ad5069400
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-21 16:22:55 -0800

    test: Log_WriteToAFile_Success

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Logger.Tests;
 
@@ -19,6 +22,21 @@ public class FileLoggerTests
         string FilePath = "text.txt";
         FileLogger FileLogger = new FileLogger(FilePath);
         Assert.AreNotEqual("notText.txt", FileLogger.GetPathName());
+    }
+
+    [TestMethod]
+    public void Log_WriteToAFile_Success()
+    {
+        string FilePath = "C:\\CSCD371\\EWU-CSCD371-2024-Winter\\Text.txt";
+        string Message = "Here is my message, please don't yeal at me";
+        FileLogger FileLogger = new FileLogger(FilePath);
+        FileLogger.Log(LogLevel.Error, Message);
+
+        StreamReader StreamReader = new StreamReader(FilePath);
+        string MessageFromStreamReader = StreamReader.ReadLine() ?? string.Empty;
+        StreamReader.Close();
+        Assert.AreEqual(Message, MessageFromStreamReader);
+
     }
 
 }
--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace Logger
         }
         public override void Log(LogLevel logLevel, string message)
         {
+            
             throw new NotImplementedException();
         }
 
@@ -22,5 +24,6 @@ namespace Logger
         {
             return _PathName;
         }
+
     }
 }
