ce3c4c32595c326fca26a36363d91ea9ad06ff01
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-21 17:57:00 -0800

    here is some code

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,26 +1,59 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Logger;
 
 public static class BaseLoggerMixins
 {
-    public static void Error(this BaseLogger logger, string message, params string[] args)
+    public static void Error(this BaseLogger? logger, string message, params string[] args)
     {
-        logger.Log(LogLevel.Error, message);
+        if(logger == null)
+        {
+            throw new ArgumentNullException("Error extention method: Missing BaseLogger");
+        }
+
+        if(args == null)
+        {
+            logger.Log(LogLevel.Error, message);
+        }
+        else
+        {
+            "Message 42";
+            "Message {0}", 42;
+
+            logger.Log(LogLevel.Error, message);
+        }
+
+        
     }
 
-    public static void Warning(this BaseLogger logger, string message, params string[] args)
+    public static void Warning(this BaseLogger? logger, string message, params string[] args)
     {
+        if (logger == null)
+        {
+            throw new ArgumentNullException("Error extention method: Missing BaseLogger");
+        }
+
         logger.Log(LogLevel.Warning, message);
     }
 
-    public static void Information(this BaseLogger logger, string message, params string[] args)
+    public static void Information(this BaseLogger? logger, string message, params string[] args)
     {
+        if (logger == null)
+        {
+            throw new ArgumentNullException("Error extention method: Missing BaseLogger");
+        }
+
         logger.Log(LogLevel.Information, message);
     }
 
-    public static void Debug(this BaseLogger logger, string message, params string[] args)
+    public static void Debug(this BaseLogger? logger, string message, params string[] args)
     {
+        if (logger == null)
+        {
+            throw new ArgumentNullException("Error extention method: Missing BaseLogger");
+        }
+
         logger.Log(LogLevel.Debug, message);
     }
 }
