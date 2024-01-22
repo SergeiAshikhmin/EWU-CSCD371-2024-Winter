7408e5624ebd36323e9b08f26956b7607ed50b05
Anna-Mclenko <amikhailenko@ewu.edu>
2024-01-21 19:42:10 -0800

    fix FileLogger build error

--- Logger/FileLogger.cs
+++ Logger/FileLogger.cs
@@ -5,27 +5,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
+namespace Logger;
+public class FileLogger : BaseLogger
 {
-    public class FileLogger : BaseLogger
+    private readonly string _PathName;
+    public FileLogger(string filePath)
     {
-        private readonly string _PathName;
-        public FileLogger(string filePath)
-        {
-            _PathName = filePath;
-        }
-        public override void Log(LogLevel logLevel, string message)
-        {
-            DateTime DateTime = DateTime.Now;
-            string ClassName = this.ClassName ?? "Null name???";
-            string MessageToAppend = $"{DateTime} {ClassName} {logLevel.ToString()} : {message}";
-            File.AppendAllText(GetPathName(), MessageToAppend + Environment.NewLine);
-        }
-
-        public string GetPathName()
-        {
-            return _PathName;
-        }
-
+        _PathName = filePath;
     }
+    public override void Log(LogLevel logLevel, string message)
+    {
+        DateTime DateTime = DateTime.Now;
+        string ClassName = this.ClassName ?? "Null name???";
+        string MessageToAppend = $"{DateTime} {ClassName} {logLevel.ToString()} : {message}";
+        File.AppendAllText(GetPathName(), MessageToAppend + Environment.NewLine);
+    }
+
+    public string GetPathName()
+    {
+        return _PathName;
+    }
+
 }
