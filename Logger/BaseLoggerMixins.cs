d3e45303825b5b414d6f7bcd4bff63653b9408fa
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-21 17:41:07 -0800

    BaseLoggerMixins: Error method extenction

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,11 @@
-﻿namespace Logger;
+﻿using System.Runtime.CompilerServices;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
-
+    public static void Error(this BaseLogger logger, string message, params string[] args)
+    {
+        logger.Log(LogLevel.Error, message);
+    }
 }
