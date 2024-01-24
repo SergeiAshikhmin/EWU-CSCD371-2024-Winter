6675b32eb9e01a472212b08eb58435131605dd68
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-23 16:22:44 -0800

    in class lecture about nullable and how to deal with it

--- ClassDemo/ClassDemo.Tests/PersonTests.cs
+++ ClassDemo/ClassDemo.Tests/PersonTests.cs
@@ -12,6 +12,46 @@ public class PersonTests
     public void Constructor_ValidName_SuccessfulInitialization()
     {
         Person person = new();
+        
+    }
+
+    [Fact]
+    public void FirstName_SetInigo_Success()
+    {
+        Person person = new();
+        person.FirstName = "Inigo";
+        Assert.Equal("Inigo", person.FirstName);
+
+    }
+
+    [Fact]
+    public void FirstName_SetFirstNameNull_Success()
+    {
+        Person person = new(null!);
+        Assert.Throws<ArgumentNullException>(
+            () => person.FirstName = null!);
+    }
+
+    [Fact]
+    public void FirstName_SetNull_Success()
+    {
+        Person? person;
+        Assert.Throws<ArgumentException>(
+            () =>
+            {
+                person = new(null!);
+            });
+    }
+
+    [Fact]
+    public void MiddleName_SetInigo_Success()
+    {
+        Person person = new("Inigo")
+        {
+            MiddleName = "Middle"
+        };
+
+        Assert.Equal("Middle", person.MiddleName);
     }
 
 }
--- ClassDemo/ClassDemo/Person.cs
+++ ClassDemo/ClassDemo/Person.cs
@@ -1,6 +1,23 @@
-﻿namespace ClassDemo;
+﻿
+namespace ClassDemo;
 
 public class Person
 {
+    private string? _firstName;
 
+    public Person(string firstName)
+    {
+        FirstName = firstName;
+    }
+
+    public string FirstName 
+    { 
+        get => _firstName!; 
+        set
+        {
+            _firstName = value ?? throw new ArgumentNullException(nameof(value));
+        } 
+    }
+
+    public string? MiddleName { get; set; }
 }
