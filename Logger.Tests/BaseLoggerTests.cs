1e86e59538a965395754835e4ea0249e79f07c34
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-01-25 16:38:11 -0800

    test: added BaseLogger auto property test

---
+++ Logger.Tests/BaseLoggerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Logger;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests;
+
+[TestClass]
+public class BaseLoggerTests
+{
+    [TestMethod]
+    public void AutoProperty_SetClassNameToNull_Success()
+    {
+        MocLogger MocLogger = new();
+        Assert.AreEqual(null, MocLogger.ClassName);
+    }
+
+    [TestMethod]
+    public void AutoProperty_SetClassNameToProperName_Sucess()
+    {
+        MocLogger MocLogger = new();
+        MocLogger.ClassName = "FileLogger";
+        Assert.AreEqual(nameof(FileLogger), MocLogger.ClassName);
+    }
+}
+
+
+public class MocLogger : BaseLogger
+{
+    public override void Log(LogLevel logLevel, string message)
+    {
+        throw new NotImplementedException();
+    }
+}
