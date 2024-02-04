6f361362a17175eb81af32c7891ad5730627ec5d
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-03 20:32:26 -0800

    test pass Record initialized

--- Logger.Tests/FullNameRecordTests.cs
+++ Logger.Tests/FullNameRecordTests.cs
@@ -8,6 +8,7 @@ public class FullNameRecordTests
     [Fact]
     public void FullName_RecordInitialized_Success()
     {
-        
+        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
+        Assert.Equal("FullNameRecord { First = Inigo, Last = Montoya, Middle = Alex }", fullName.ToString());
     }
 }
