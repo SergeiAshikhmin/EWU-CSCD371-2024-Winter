a4a04f792c99fcb11183a1c31d1aae76ab3a9162
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-04 21:14:29 -0800

    BookTests simplified

--- Logger.Tests/BookTests.cs
+++ Logger.Tests/BookTests.cs
@@ -12,14 +12,14 @@ namespace Logger.Tests
         [Fact]
         public void BookRecord_InitilizeName_Success()
         {
-            BookRecord book = new BookRecord("My Book");
+            BookRecord book = new("My Book");
             Assert.Equal("My Book", book.Name);
         }
 
         [Fact]
         public void BookRecord_SameBookEquals_Success()
         {
-            BookRecord book1 = new BookRecord("Is it my book?");
+            BookRecord book1 = new("Is it my book?");
             BookRecord book2 = book1 with { };
             Assert.True(book1.Equals(book2));   
         }
@@ -27,8 +27,8 @@ namespace Logger.Tests
         [Fact]
         public void BookRecord_TwoBooksNotEquals_Success()
         {
-            BookRecord book1 = new BookRecord("Narnia");
-            BookRecord book2 = new BookRecord("Narnia");
+            BookRecord book1 = new("Narnia");
+            BookRecord book2 = new("Narnia");
             Assert.False(book1.Equals(book2));
         }
     }
