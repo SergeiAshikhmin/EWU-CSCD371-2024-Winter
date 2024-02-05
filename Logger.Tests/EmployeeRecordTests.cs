8c9ceb9aee93f96540ccc82a1f2f95bc9a0e29fb
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-04 22:33:50 -0800

    EmployeeRecord and Tests for it

--- Logger.Tests/EmployeeRecordTests.cs
+++ Logger.Tests/EmployeeRecordTests.cs
@@ -5,5 +5,10 @@ namespace Logger.Tests;
 
 public class EmployeeRecordTests
 {
-    
+    [Fact]
+    public void EmployeeRecord_InitializeObject_Success()
+    {
+        EmployeeRecord employeeRecord = new("Sergei", "Ashikhmin", "D");
+        Assert.Equal("Sergei D Ashikhmin", employeeRecord.Name);
+    }
 }
--- Logger/EmployeeRecord.cs
+++ Logger/EmployeeRecord.cs
@@ -4,6 +4,21 @@ namespace Logger;
 
 public record class EmployeeRecord : IEntity
 {
-    public Guid ID { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
-    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public EmployeeRecord(string First, string Last, string? Middle)
+    {
+        FullName = new(First, Last, Middle);
+        
+        if(String.IsNullOrEmpty(Middle))
+        {
+            Name = First + " " + Last;
+        }
+        else
+        {
+            Name = First + " " + Middle + " " + Last;
+        }
+    }
+    public Guid ID { get; init; } = typeof(EmployeeRecord).GUID;
+    public string Name { get; set; }
+
+    public FullNameRecord FullName { get; init; }
 }
