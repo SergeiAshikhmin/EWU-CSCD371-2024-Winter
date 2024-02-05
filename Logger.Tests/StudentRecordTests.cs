899cc4b25ee8b3943be36eacc885ef55389f015d
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-04 22:37:28 -0800

    student record and tests

--- Logger.Tests/StudentRecordTests.cs
+++ Logger.Tests/StudentRecordTests.cs
@@ -5,5 +5,26 @@ namespace Logger.Tests;
 
 public class StudentRecordTests
 {
-    
+    [Fact]
+    public void StudentRecord_getName_Success()
+    {
+        StudentRecord studentRecord = new("Inigo", "Montoya", "Alex");
+        Assert.Equal("Inigo Alex Montoya", studentRecord.Name);
+    }
+
+    [Fact]
+    public void StudentRecord_SameStudentEquals_Success()
+    {
+        StudentRecord student1 = new("Inigo", "Montoya", "Alex");
+        StudentRecord student2 = student1 with {};
+        Assert.True(student1.Equals(student2));
+    }
+
+    [Fact]
+    public void StudentRecord_TwoStudentsNotEqual_Success()
+    {
+        StudentRecord student1 = new("Inigo", "Montoya", "Alex");
+        StudentRecord student2 = new("Inigo", "Montoya", "Alex");
+        Assert.False(student1.Equals(student2));
+    }
 }
