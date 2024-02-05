105ae63f657a9b1bfdc908725cef94c3d2fe97f0
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-04 19:53:12 -0800

    added entity capabilities

--- Logger/EmployeeRecord.cs
+++ Logger/EmployeeRecord.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace Logger;
 
-public record class EmployeeRecord
+public record class EmployeeRecord : IEntity
 {
-    
+    public Guid Id { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
--- Logger/StudentRecord.cs
+++ Logger/StudentRecord.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace Logger;
 
-public record class StudentRecord
+public record class StudentRecord : IEntity
 {
-    
+    public Guid Id { get => throw new NotImplementedException(); init => throw new NotImplementedException(); }
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
