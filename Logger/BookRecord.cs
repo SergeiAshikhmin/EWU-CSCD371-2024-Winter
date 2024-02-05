cddb93e9ee00337617c0c0d5f2ad6df7fcb50833
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-05 15:59:33 -0800

    remove comment in BookRecord

--- Logger/BookRecord.cs
+++ Logger/BookRecord.cs
@@ -11,7 +11,5 @@ namespace Logger
         public Guid Id { get; init; } = Guid.NewGuid();
         string IEntity.Name { get; set; } = Name;
 
-        //public string Name { get; init; } = Name ?? throw new ArgumentNullException(nameof(Name));
-
     }
 }
