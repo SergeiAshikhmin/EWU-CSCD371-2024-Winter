db2f4d79e8f55d7ac4108110d0840c1c385d0ea9
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-05 17:13:15 -0800

    equality tests for employee

--- Logger.Tests/EmployeeRecordTests.cs
+++ Logger.Tests/EmployeeRecordTests.cs
@@ -8,7 +8,23 @@ public class EmployeeRecordTests
     [Fact]
     public void EmployeeRecord_InitializeObject_Success()
     {
-        EmployeeRecord employeeRecord = new("Sergei", "Ashikhmin", "D");
-        Assert.Equal("Sergei D Ashikhmin", employeeRecord.Name);
+        EmployeeRecord employeeRecord = new("Inigo", "Montoya", "Alex");
+        Assert.Equal("Inigo Alex Montoya", employeeRecord.Name);
+    }
+
+    [Fact]
+    public void EmployeeRecord_SameEmployee_Success()
+    {
+        EmployeeRecord employee1 = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employee2 = employee1 with {};
+        Assert.True(employee1.Equals(employee2));
+    }
+
+    [Fact]
+    public void EmployeeRecord_TwoEmployeesNotEqual_Success()
+    {
+        EmployeeRecord employee1 = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employee2 = new("Inigo", "Montoya", "Alex");
+        Assert.False(employee1.Equals(employee2));
     }
 }
