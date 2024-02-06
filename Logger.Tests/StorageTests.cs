118996110603e559b616e2dc22df46c098676227
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-05 21:29:48 -0800

    added tests for Emplyee starage

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -5,15 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Logger.Tests
+namespace Logger.Tests;
+public class StorageTests
 {
-    public class StorageTests
+    // Book
+
+    // Student
+
+    // Employee
+    [Fact]
+    public void Add_EmployeRecord_Success()
     {
-        // Book
+        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employeeRecord = new(fullName);
+        Storage storage = new();
+        storage.Add(employeeRecord);
+        Assert.True(storage.Contains(employeeRecord));
+    }
 
-        // Student
+    [Fact]
+    public void Remove_EmployeRecord_Success()
+    {
+        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employeeRecord = new(fullName);
+        Storage storage = new();
+        storage.Add(employeeRecord);
+        storage.Remove(employeeRecord);
+        Assert.False(storage.Contains(employeeRecord));
+    }
 
-        // Employee
+    [Fact]
+    public void Contains_EmployeeRecord_Success()
+    {
+        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employeeRecord = new(fullName);
+        EmployeeRecord employeeRecord1 = new(fullName);
+        Storage storage = new();
+        storage.Add(employeeRecord);
+        storage.Add(employeeRecord1);
+        Assert.True(storage.Contains(employeeRecord));
+    }
 
+    [Fact]
+    public void Get_EmployeeRecord_Success()
+    {
+        FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
+        EmployeeRecord employeeRecord = new(fullName);
+        EmployeeRecord employeeRecord1 = new(fullName);
+        Storage storage = new();
+        storage.Add(employeeRecord);
+        storage.Add(employeeRecord1);
+        Assert.Equal(employeeRecord, storage.Get(employeeRecord.Id));
     }
 }
