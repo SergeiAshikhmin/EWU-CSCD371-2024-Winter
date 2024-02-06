c60c96e6d253d3609cbc6005b3695ee99f9804ae
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-05 16:36:09 -0800

    comment on where the refactored code is

--- Logger/EmployeeRecord.cs
+++ Logger/EmployeeRecord.cs
@@ -2,4 +2,5 @@ using System;
 
 namespace Logger;
 
+// The Emplyee record inperits the PresonRecord where all of the refactored code was moved.
 public record class EmployeeRecord(string First, string Last, string? Middle) : PersonRecord(First, Last, Middle);
