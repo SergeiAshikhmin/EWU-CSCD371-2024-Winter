10f74f6269976dcd1050f70c6e26d96879c75460
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-05 21:22:28 -0800

    StorageTests got new tests for BookRecord

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -11,7 +11,43 @@ namespace Logger.Tests
     {
         // Book
 
-        // Student
+
+        // StudentRecord
+        [Fact]
+        public void Add_StudentRecord_Success()
+        {
+            BookRecord record = new("The Great Gatsby");
+            
+
+            Storage storage = new();
+            storage.Add(record);
+
+            Assert.True(storage.Contains(record));
+        }
+
+        [Fact]
+        public void Remove_StudentRecord_Success()
+        {
+            BookRecord record = new("To Kill a Mockingbird");
+
+            Storage storage = new();
+            storage.Add(record);
+
+            storage.Remove(record);
+
+            Assert.False(storage.Contains(record));
+        }
+
+        [Fact]
+        public void Get_ReturnsCorrectId_Success()
+        {
+            BookRecord record = new("Moby-Dick");
+
+            Storage storage = new();
+            storage.Add(record);
+
+            Assert.Equal<IEntity>(record, storage.Get(record.Id));
+        }
 
         // Employee
 
