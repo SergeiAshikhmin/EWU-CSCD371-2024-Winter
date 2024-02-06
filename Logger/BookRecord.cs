ce91c12a6f7a2d58c915abbe6ef9cda684e0203e
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-05 21:54:31 -0800

    fix build err

--- Logger/BookRecord.cs
+++ Logger/BookRecord.cs
@@ -4,16 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Logger
-{
-    public record class BookRecord(string Name) : IEntity
-    {
-        // we implemented implicitly:
-        // because each BookRecord object needs to have the id accessable in order to use the Get() function.
-        public Guid Id { get; init; } = Guid.NewGuid();
-        // we implemented explicity:
-        // because the Book record and the IEntity both have a duplicated naming convention for the Property Name.
-        string IEntity.Name { get; set; } = Name;
+namespace Logger;
 
-    }
+public record class BookRecord(string Name) : IEntity
+{
+    // we implemented implicitly:
+    // because each BookRecord object needs to have the id accessable in order to use the Get() function.
+    public Guid Id { get; init; } = Guid.NewGuid();
+    // we implemented explicity:
+    // because the Book record and the IEntity both have a duplicated naming convention for the Property Name.
+    string IEntity.Name { get; set; } = Name;
+
 }
