6bc877efa3b72aee3fd0ad6fbcbf5cc9a7680385
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-05 16:35:23 -0800

    add comments in StudentRecord

--- Logger/StudentRecord.cs
+++ Logger/StudentRecord.cs
@@ -3,4 +3,6 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace Logger;
 
+// A StudentRecord class inherits from the PersonRecord base class and passes a constructor call to it.
+// For this class implementation, see PersonRercord
 public record class StudentRecord(string First, string Last, string? Middle) : PersonRecord(First, Last, Middle);
