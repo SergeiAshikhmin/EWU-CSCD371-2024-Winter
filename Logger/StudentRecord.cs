348763f7d5e883f79c55d3fc453744d85b53429d
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-06 17:13:05 -0800

    Student refactor

--- Logger/StudentRecord.cs
+++ Logger/StudentRecord.cs
@@ -5,4 +5,9 @@ namespace Logger;
 
 // A StudentRecord class inherits from the PersonRecord base class and passes a constructor call to it.
 // For this class implementation, see PersonRecord
-public record class StudentRecord(FullNameRecord FullName) : PersonRecord(FullName);
+public record class StudentRecord(FullNameRecord FullName) : PersonRecord(FullName)
+{
+    public StudentRecord(string First, string Last, string Middle) : this(new FullNameRecord(First, Last, Middle)) { }
+
+    public StudentRecord(string First, string Last) : this(new FullNameRecord(First, Last)) { }
+}
