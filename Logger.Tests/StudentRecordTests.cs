e39b3cdff954368ec8313eadaa52dc0ea4ec7eaa
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-06 17:19:10 -0800

    new tests for new constructors in StudentRecord

--- Logger.Tests/StudentRecordTests.cs
+++ Logger.Tests/StudentRecordTests.cs
@@ -6,7 +6,7 @@ namespace Logger.Tests;
 public class StudentRecordTests
 {
     [Fact]
-    public void StudentRecord_getName_Success()
+    public void StudentRecord_GetName_Success()
     {
         FullNameRecord fullName = new("Inigo", "Montoya", "Alex");
         StudentRecord studentRecord = new(fullName);
@@ -29,5 +29,19 @@ public class StudentRecordTests
         StudentRecord student1 = new(fullName);
         StudentRecord student2 = new(fullName);
         Assert.False(student1.Equals(student2));
+    }
+
+    [Fact]
+    public void StudentRecord_SetFirstLastWithString_Success()
+    {
+        StudentRecord studentRecord = new("Inigo", "Montoya");
+        Assert.Equal("Inigo Montoya", studentRecord.Name);
+    }
+
+    [Fact]
+    public void StudentRecord_SetFirstLastMiddleWithString_Success()
+    {
+        StudentRecord record = new("Inigo", "Montoya", "Alex");
+        Assert.Equal("Inigo Alex Montoya", record.Name);
     }
 }
