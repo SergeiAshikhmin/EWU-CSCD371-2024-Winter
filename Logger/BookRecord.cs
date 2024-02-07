6590d26225ecd668242a264a34814c8cd12c3040
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-06 17:10:12 -0800

    Book refactor

--- Logger/BookRecord.cs
+++ Logger/BookRecord.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Logger;
 
-public record class BookRecord(Guid Id, string Name) : IEntity
+public record class BookRecord(string Name) : IEntity
 {
     // we implemented implicitly:
     // because each BookRecord object needs to have the id accessible in order to use the Get() function.
-    public Guid Id { get; init; } = Id;
+    public Guid Id { get; init; } = Guid.NewGuid();
     // we implemented explicitly:
     // because the Book record and the IEntity both have a duplicated naming convention for the Property Name.
     string IEntity.Name { get; } = Name;
