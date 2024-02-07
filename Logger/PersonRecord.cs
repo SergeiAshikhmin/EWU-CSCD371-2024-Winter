16a746d7f728a8c92bef5abc27ebb9cf6998d327
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-06 17:07:47 -0800

    PersonRecord refactor

--- Logger/PersonRecord.cs
+++ Logger/PersonRecord.cs
@@ -8,12 +8,20 @@ namespace Logger;
 
 public record class PersonRecord(FullNameRecord FullName) : IEntity
 {
-    // Implicit Iplementation:
+    public PersonRecord(string First, string Last, string Middle) : this(new FullNameRecord(First, Last, Middle))
+    {
+
+    }
+
+    public PersonRecord(string First, string Last) : this(new FullNameRecord(First, Last))
+    { 
+    }
+    // Implicit Implementation:
     // Because StudentRecord is an Entity that logically has an Id property.
     public Guid Id { get; init; } = Guid.NewGuid();
 
     // Implicit Implementation:
     // Because it makes logical sense for the StudentRecord to have easy access to the name property.
-    public string Name { get; set; } = FullName.ToString();
+    public string Name { get; } = FullName.ToString();
 
 }
