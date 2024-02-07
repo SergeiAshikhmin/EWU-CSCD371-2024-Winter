0387975d6893c6318d7aaf49168878f3d98dee0c
Anna-Mclenko <amikhailenko@ewu.edu>
2024-02-06 17:08:28 -0800

    deleted uneeded test

--- Logger.Tests/StudentRecordTests.cs
+++ Logger.Tests/StudentRecordTests.cs
@@ -30,14 +30,4 @@ public class StudentRecordTests
         StudentRecord student2 = new(fullName);
         Assert.False(student1.Equals(student2));
     }
-
-    [Fact]
-    public void StudentRecord_ChangeName_Success()
-    {
-        FullNameRecord fullName = new("Inigo", "Montoya", "");
-        StudentRecord record = new(fullName);
-
-        FullNameRecord fullNameRecord = new("Harry", "Potter", null);
-        record.Name = "Sergei";
-    }
 }
