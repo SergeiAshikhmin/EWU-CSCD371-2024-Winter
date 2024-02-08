5ac3cfc72b4ea587bb972af17ac5d250b8d4b746
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-02-08 13:52:05 -0800

    Remove using in FullNameRecord

--- Logger/FullNameRecord.cs
+++ Logger/FullNameRecord.cs
@@ -1,12 +1,10 @@
-using System;
-
 namespace Logger;
 //Why its a Value Type:
 // The reason we decided to use a value type is because it logically represents a single value.
 // Another reason this is a value type is because this record does not need to be inherited.
 //Why its Immutable:
 // The reason this struct is immutable is because it is a value type and value types must be immutable. 
-// Here the readonly modifyer ensures that the record is immutable.
+// Here the readonly modifier ensures that the record is immutable.
 public readonly record struct FullNameRecord(string First, string Last, string? Middle = null)
 {
     public readonly string First { get; init; } = First ?? throw new ArgumentNullException(nameof(First));
