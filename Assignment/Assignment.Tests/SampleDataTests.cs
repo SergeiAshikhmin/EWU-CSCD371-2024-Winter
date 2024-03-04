7bc57ad2d86d401210f124aa2b99408219d5a89f
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-03-03 16:02:20 -0800

    Setting things up

---
+++ Assignment/Assignment.Tests/SampleDataTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class SampleDataTests
+{
+    [TestMethod]
+    public void CsvRows()
+    {
+        Assert.IsTrue(TestingData.Data.Contains("Sergei"));
+    }
+
+}
