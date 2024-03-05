6d654c6dc0feab4efd749887d1bab781cf786f81
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-03-05 13:31:50 -0800

    Node implements IEnumerable

---
+++ Assignment/Assignment/Node.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace Assignment;
+
+public class Node<T> : IEnumerable<Node<T>>
+{
+    public T Data { get; set; }
+    public Node(T data)
+    {
+        Data = data;
+        Next = this;
+    }
+    public Node<T> Next { get; private set; }
+
+    public void Append(T data)
+    {
+
+        if (Exists(data))
+        {
+            throw new ArgumentException("The value already exists");
+        }
+
+        Node<T> cur = this;
+
+        Node<T> nextNode = new(data)
+        {
+            Next = cur.Next
+        };
+        cur.Next = nextNode;
+    }
+
+    public override string ToString()
+    {
+        return Data!.ToString()!;
+    }
+
+    public bool Exists(T data)
+    {
+
+        Node<T> cur = this;
+
+        do
+        {
+            if (cur.Data!.Equals(data))
+            {
+                return true;
+            }
+
+            cur = cur.Next;
+
+        } while (cur != this);
+
+        return false;
+    }
+
+    public void Clear()
+    {
+        //Yur do not need to worry about Garbage collection if implemented like below.
+        //The reason for this is because there does not exist a root reference to the existing object anymore.
+        //Thus, the garbage collector will automatically collect the other nodes.
+        Node<T> curr = this;
+
+        curr.Next = this;
+    }
+
+    public IEnumerator<Node<T>> GetEnumerator()
+    {
+        Node<T> current = this;
+
+        do
+        {
+            yield return current.Next;
+            current = current.Next;
+        } while (current != this);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public IEnumerable<Node<T>> ChildItems(int maximum)
+    {
+        int n = 0;
+        return Next.TakeWhile(node => n++ < maximum);
+    }
+}
---
+++ Assignment/AssignmentXunitTests/NodeTests.cs
@@ -0,0 +1,120 @@
+using Assignment;
+using System.Reflection;
+
+namespace AssignmentXunitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Node_NextPointToItself_Success()
+        {
+            Node<string> node = new("Inigo Montoya");
+            Assert.Equal(node.Data, node.Next.Data);
+        }
+
+        [Fact]
+        public void Node_NextReferencePointToSecondNode_Success()
+        {
+            Node<int> node = new(1);
+            node.Append(2);
+            Assert.NotEqual(node.Data, node.Next.Data);
+        }
+
+        [Fact]
+        public void Node_NextPointToTheLastNode_Success()
+        {
+            Node<double> node = new(1.0);
+            node.Append(2.0);
+            node.Append(3.0);
+            Assert.Equal(2.0, node.Next.Next.Data);
+        }
+
+        [Fact]
+        public void Node_NextReferencePointToFistNode_Success()
+        {
+            Node<string> node = new("Inigo Montoya");
+            node.Append("Butter Cup");
+            Assert.Equal("Inigo Montoya", node.Next.Next.Data);
+        }
+
+        [Fact]
+        public void Exists_NodeContainsData_True()
+        {
+            Node<int> node = new(1);
+            node.Append(2);
+            node.Append(3);
+            Assert.True(node.Exists(3));
+        }
+
+        [Fact]
+        public void Exists_NodeDoesNotContainsData_True()
+        {
+            Node<double> node = new(1.1);
+            node.Append(2.2);
+            node.Append(3.3);
+            Assert.False(node.Exists(4.4));
+        }
+
+        [Fact]
+        public void ToString_PrintList_Success()
+        {
+            Node<string> node = new("Inigo Montoya");
+            node.Append("Butter Cup");
+            node.Append("Prince Johan");
+            Assert.Equal("Inigo Montoya", node.ToString());
+        }
+
+        [Fact]
+        public void Append_ExistedValue_ThrowArgumentException()
+        {
+            Node<int> node = new(1);
+            node.Append(2);
+            node.Append(3);
+            Assert.Throws<ArgumentException>(
+                () => node.Append(1));
+
+        }
+
+        [Fact]
+        public void Clear_NodesDeleted_Successful()
+        {
+            Node<double> node = new(1.1);
+            node.Append(2.1);
+            node.Append(3.1);
+            node.Clear();
+            Assert.Equal(node.Data, node.Next.Data);
+        }
+
+        [Fact]
+        public void GetEnumerator()
+        {
+            Node<string> node = new("Inigo Montoya");
+            node.Append("Sergei Ashikhmin");
+            node.Append("Batman");
+            node.Append("Dmitrii");
+            node.Append("David");
+            node.Append("Artem");
+
+            IEnumerable<Node<string>> items = node.OrderByDescending(node => node.Data);
+
+            Assert.Equal(6, items.Count());
+            Assert.Equal("Sergei Ashikhmin", items.First().Data);
+
+
+        }
+
+        [Fact]
+        public void ChildItems()
+        {
+            Node<string> node = new("Inigo Montoya");
+            node.Append("Sergei Ashikhmin");
+            node.Append("Batman");
+            node.Append("Dmitrii");
+            node.Append("David");
+            node.Append("Artem");
+
+            IEnumerable<Node<string>> nodes = node.ChildItems(3).OrderBy(node => node.Data);
+            Assert.Equal(3, nodes.Count());
+        }
+    }
+}
